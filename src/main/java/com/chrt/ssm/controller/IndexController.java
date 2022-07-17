048f4e4207a9d06dd1fb69391e72317fbe38b4ba
chrt <3378310437@qq.com>
2022-07-17 20:58:46 +0800

    change the pom.xml and add information for IndexController

--- src/main/java/com/chrt/ssm/controller/IndexController.java
+++ src/main/java/com/chrt/ssm/controller/IndexController.java
@@ -3,13 +3,24 @@ package com.chrt.ssm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 用于控制首页位置以及hello页面
+ */
 @Controller
 public class IndexController {
+    /**
+     * 配置首页位置
+     * @return 首页位置
+     */
     @RequestMapping("/")
     public String toIndex() {
         return "index";
     }
 
+    /**
+     * 配置hello页面位置
+     * @return hello页面位置
+     */
     @RequestMapping("/hello")
     public String toHello() {
         return "hello";
